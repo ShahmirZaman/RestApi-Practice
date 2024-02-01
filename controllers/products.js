4b44a470b113a9e214438262e3ee0c33f53b2b27
Shahmir Zaman <shahmirzaman009@gmail.com>
2024-02-01 17:32:35 +0500

    host api

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,7 @@
 import product from "../models/product.js"
 
 const getAllProducts = async(req,res) => {
-    const { company, name, featured } = req.query;
+    const { company, name, featured, sort, select } = req.query;
     const queryObj = {};
     if(company) {
         queryObj.company = company;
@@ -12,17 +12,34 @@ const getAllProducts = async(req,res) => {
     if(featured){
         queryObj.featured = featured;
     }
+    let apiData = product.find(queryObj);
+
+    if(sort){
+        let sortFix = sort.split(",").join(" ")
+        apiData = apiData.sort(sortFix);
+    }
+    if(select){
+        let selectFix = select.split(",").join(" ")
+        apiData = apiData.select(selectFix);
+    }
+    let page = Number(req.query.page) || 1;
+    let limit = Number(req.query.limit) || 3;
+    let skip = (page - 1) * limit;
+    apiData = apiData.skip(skip).limit(limit);
+
     console.log(queryObj);
-    const myData = await product.find(queryObj)
+    const products = await apiData;
     res.status(200).json({
-        myData
+        products,
+        nbHits:products.length
     })
 }
 const getAllProductsTesting = async(req,res) => {
-    const myData = await product.find(req.query)
+    console.log(req.query)
+    const products = await product.find(req.query)
     res.status(200).json({
         status:true,
-        myData,
+        products,
     })
 }
 
